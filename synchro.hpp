267b0b8e2045275e51c7f2dab367b0baa6c8ea91
prog-fh <prog-fh@users.noreply.github.com>
2019-06-03 14:18:25 +0200

    added synchro

---
+++ synchro.hpp
@@ -0,0 +1,69 @@
+//----------------------------------------------------------------------------
+
+#ifndef DIM_SYNCHRO_HPP
+#define DIM_SYNCHRO_HPP 1
+
+#include <atomic>
+
+namespace dim {
+
+class Synchro
+{
+public:
+
+  using sync_t = unsigned int; // overflow is correct
+
+  Synchro()
+  : sync_{}
+  , ack_count_{}
+  {
+    // nothing more to be done
+  }
+
+  void
+  sync(int thread_count)
+  {
+    ack_count_.store(thread_count-1, std::memory_order_release);
+    sync_.fetch_add(1, std::memory_order_release);
+  }
+
+  void
+  wait_for_sync(sync_t &last_sync)
+  {
+    for(;;)
+    {
+      if(const auto sync=sync_.load(std::memory_order_acquire);
+         last_sync!=sync)
+      {
+        last_sync=sync;
+        break;
+      }
+    }
+  }
+
+  void
+  ack()
+  {
+    ack_count_.fetch_sub(1, std::memory_order_release);
+  }
+
+  void
+  wait_for_ack()
+  {
+    while(ack_count_.load(std::memory_order_acquire)!=0)
+    {
+      // busy wait
+    }
+  }
+
+private:
+  int thread_count_;
+  std::atomic<sync_t> sync_;
+  std::atomic<int> ack_count_;
+};
+
+} // namespace dim
+
+#endif // DIM_SYNCHRO_HPP
+
+//----------------------------------------------------------------------------
